9a9b1138a51e371b9fd8974d0a75b0c6bb1aeecb
Curtis Wright <cwright8071@gmail.com>
2020-09-20 20:58:46 -0400

    updating files one last time

--- Homework/Instructions/StarterCode/static/js/app.js
+++ Homework/Instructions/StarterCode/static/js/app.js
@@ -3,18 +3,16 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-var $tbody = d3.select("tbody");
-var inputDate = d3.select("#datetime");
-var inputCity = d3.select("#city");
-var button = d3.select("#filter-btn");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+var $tbody=d3.select("tbody");
+var inputDate=d3.select("#datetime");
+var button=d3.select("#filter-btn");
+var columns=["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 
 
 
-var populateTable = (dataInput) => {
-    dataInput.forEach(ufoSighting => {
-        var row = $tbody.append("tr");
+var populateTable=(dataInput) => {
+    dataInput.forEach(ufoSighting => {var row=$tbody.append("tr");
         columns.forEach(column => row.append("td").text(ufoSighting[column])
         )
     });
@@ -23,22 +21,21 @@ var populateTable = (dataInput) => {
 populateTable(tableData)
 
 button.on("click", ()=> {
-    d3.event.preventDefaul();
+    d3.event.preventDefault();
     var inputtedDate=inputDate.property("value").trim();
     var inputtedCity=inputCity.property("value").trim();
     var filterDate=tableData.filter(tableData=>tableData.datetime==inputtedDate);
-    var filterCity=tableData.filter(tableData=>tableData.city==inputtedCity);
-    var filterCombineData=tableData.filter(tableData=>tableData.datetime==inputtedDate && tableData.city==inputtedCity);
+    var filterCombinedData = tableData.filter(tableData => tableData.datetime === inputtedDate);
     $tbody.html("");
-
     let response = {
-        filterDate, filterCity, filterCombinedData
+        filterDate, filterCombinedData
     }
-
     
-    if(response.filterCombinedData.length !== 0) {
+    if(response.filterDate.length !== 0) {
         addData(filterCombinedData);
     }
+
+
 
     
         else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
@@ -48,7 +45,7 @@ button.on("click", ()=> {
         
 
         else {
-            $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+            $tbody.append("tr").append("td").text("Sorry, we couldn't find any sightings on this date. Please enter another!");
         }
 }
 )
